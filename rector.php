81d6a6b7d83a68945d42446b8fd81039eaa0d316
Sacha Telgenhof <me@sachatelgenhof.com>
2026-01-15 22:25:59 +0900

    build: correct minimum PHP version for Rector
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- rector.php
+++ rector.php
@@ -33,7 +33,7 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
-        SetList::PHP_83,
+        SetList::PHP_82,
         SetList::TYPE_DECLARATION,
     ]);
 };
